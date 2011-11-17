fc80e35f5a7f94ee49c6d1b4e4a46ac78f5f1791
Ernesto Arroyo <earroyoron@yahoo.es>
2011-11-17 22:00:49 +0100

    Movemos amountFor a la clase Rental

--- src/kata/Customer.java
+++ src/kata/Customer.java
@@ -50,25 +50,10 @@ class Customer {
 	 * determine amounts for each line
 	 * @param aRental
 	 * @return
+	 * @deprecated Use {@link kata.Rental#amountFor()} instead
 	 */
 	private double amountFor(Rental aRental) {
-		double thisAmount = 0;
-		switch (aRental.getMovie().getPriceCode()) {
-		case Movie.REGULAR:
-			thisAmount += 2;
-			if (aRental.getDaysRented() > 2)
-				thisAmount += (aRental.getDaysRented() - 2) * 1.5;
-			break;
-		case Movie.NEW_RELEASE:
-			thisAmount += aRental.getDaysRented() * 3;
-			break;
-		case Movie.CHILDRENS:
-			thisAmount += 1.5;
-			if (aRental.getDaysRented() > 3)
-				thisAmount += (aRental.getDaysRented() - 3) * 1.5;
-			break;
-		}
-		return thisAmount;
+		return aRental.amountFor();
 	}
 }
 
--- src/kata/Rental.java
+++ src/kata/Rental.java
@@ -16,4 +16,28 @@ class Rental {
 	public Movie getMovie() {
 		return _movie;
 	}
+
+	/**
+	 * determine amounts for each line
+	 * @return
+	 */
+	double amountFor() {
+		double thisAmount = 0;
+		switch (getMovie().getPriceCode()) {
+		case Movie.REGULAR:
+			thisAmount += 2;
+			if (getDaysRented() > 2)
+				thisAmount += (getDaysRented() - 2) * 1.5;
+			break;
+		case Movie.NEW_RELEASE:
+			thisAmount += getDaysRented() * 3;
+			break;
+		case Movie.CHILDRENS:
+			thisAmount += 1.5;
+			if (getDaysRented() > 3)
+				thisAmount += (getDaysRented() - 3) * 1.5;
+			break;
+		}
+		return thisAmount;
+	}
 }
